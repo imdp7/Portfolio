cd77c72764780340c4c3680c7316a53d3c3f284e
Darshan Patel <patel7darshan97@gmail.com>
2024-07-07 06:59:25 -0400

    prettier fix

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,54 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Footer extends Component {
-	render() {
-		if (this.props.data) {
-			var networks = this.props.data.social.map(function (network) {
-				return (
-					<li key={network.name}>
-						<a href={network.url}>
-							<i className={network.className}></i>
-						</a>
-					</li>
-				);
-			});
-		}
+  render() {
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function (network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url}>
+              <i className={network.className}></i>
+            </a>
+          </li>
+        );
+      });
+    }
 
-		return (
-			<footer>
-				<div className='row'>
-					<div className='twelve columns'>
-						<ul className='social-links'>{networks}</ul>
+    return (
+      <footer>
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">{networks}</ul>
 
-						<ul>
-							<li>
-								Modified with love by the Imdp7{' '}
-								<span
-									role='img'
-									aria-label='Connect'>
-									♥️
-								</span>
-							</li>
-						</ul>
+            <ul>
+              <li>
+                Modified with love by the Imdp7{" "}
+                <span role="img" aria-label="Connect">
+                  ♥️
+                </span>
+              </li>
+            </ul>
 
-						<ul className='copyright'>
-							<li>
-								&copy; All credit goes to Darshan Patel - Copyright 2023 IMDP7
-							</li>
-						</ul>
-					</div>
-					<div id='go-top'>
-						<a
-							className='smoothscroll'
-							title='Back to Top'
-							href='#home'>
-							<i className='icon-up-open'></i>
-						</a>
-					</div>
-				</div>
-			</footer>
-		);
-	}
+            <ul className="copyright">
+              <li>
+                &copy; All credit goes to Darshan Patel - Copyright 2023 IMDP7
+              </li>
+            </ul>
+          </div>
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,48 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Portfolio extends Component {
   render() {
-
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-        <a href={projects.url} title={projects.title}>
-           <div className="item-wrap">
-            
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
+    if (this.props.data) {
+      var projects = this.props.data.projects.map(function (projects) {
+        var projectImage = "images/portfolio/" + projects.image;
+        return (
+          <div key={projects.title} className="columns portfolio-item">
+            <a href={projects.url} title={projects.title}>
+              <div className="item-wrap">
+                <img alt={projects.title} src={projectImage} />
+                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
+                    <h5>{projects.title}</h5>
+                    <p>{projects.category}</p>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
+                <div className="link-icon">
+                  <i className="fa fa-link"></i>
+                </div>
+              </div>
+            </a>
+            <div className="portfolio-title">
+              <h3>{projects.title}</h3>
+              <a target="_blank" href={projects.github} className="button">
+                <i className="fa fa-github"></i> &nbsp;Github
+              </a>
+            </div>
           </div>
-          </a>
-          <div className="portfolio-title">
-          <h3>{projects.title}</h3>
-            <a target="_blank" href={projects.github} className="button"><i className="fa fa-github"></i>{" "}&nbsp;Github</a>
-          </div>
-        </div>
-      })
+        );
+      });
     }
 
     return (
       <section id="portfolio">
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
+        <div className="row">
+          <div className="twelve columns collapsed">
             <h1>Check Out Some of My Works.</h1>
 
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+            >
+              {projects}
             </div>
           </div>
-      </div>
-   </section>
+        </div>
+      </section>
     );
   }
 }
